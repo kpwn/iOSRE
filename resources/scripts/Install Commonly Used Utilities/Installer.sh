a7d21a6114d280d98f162ca5961e9848371933a7
Naville <403799106@qq.com>
2016-06-01 14:18:25 +0800

    Fix Theos EnvVar

--- resources/scripts/Install Commonly Used Utilities/Installer.sh
+++ resources/scripts/Install Commonly Used Utilities/Installer.sh
@@ -8,7 +8,7 @@ brew install dpkg
 echo "Installing ldid"
 brew install ldid
 echo "Installing Theos"
-echo "export THEOS=\"/opt/theos\"" >>~/.bash_profile
+echo "export THEOS=/opt/theos" >>~/.bash_profile
 source ~/.bash_profile
 sudo git clone https://github.com/theos/theos.git $THEOS
 sudo git clone https://github.com/theos/headers.git $THEOS/Headers
@@ -16,6 +16,7 @@ sudo cp -r $THEOS/Headers/ $THEOS/include
 sudo rm -rf $THEOS/Headers/
 echo "Installing wget"
 brew install wget
+:'
 echo "Installing Reveal"
 wget http://download.revealapp.com/Reveal.app.zip
 unzip -XK ./Reveal.app.zip
@@ -27,6 +28,7 @@ echo "Installing Hopper"
 sudo ./Hopper.py
 unzip -XK ./Hoppper-Latest.zip
 mv './Hopper Disassembler v3.app' '/Applications/Hopper Disassembler v3.app'
+'
 sudo ./debugserver.py
 git clone https://github.com/nygard/class-dump.git class-dump
 xcodebuild -project ./class-dump/class-dump.xcodeproj #Compile classdump
