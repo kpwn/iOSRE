eaaf31554ed881636d54ed6764667c326254732b
Naville <403799106@qq.com>
2016-06-01 12:59:27 +0800

    Fix Scripts

--- resources/scripts/Install Commonly Used Utilities/Installer.sh
+++ resources/scripts/Install Commonly Used Utilities/Installer.sh
@@ -21,11 +21,14 @@ wget http://download.revealapp.com/Reveal.app.zip
 unzip -XK ./Reveal.app.zip
 chmod +x ./Reveal.app/Contents/MacOS/Reveal 
 mv ./Reveal.app /Applications/Reveal.app
+rm -rf ./Reveal.app
+rm -rf ./Reveal.app.zip
 echo "Installing Hopper"
-./Hopper.py
+sudo ./Hopper.py
 unzip -XK ./Hoppper-Latest.zip
 mv './Hopper Disassembler v3.app' '/Applications/Hopper Disassembler v3.app'
-./debugserver.py
+sudo ./debugserver.py
 git clone https://github.com/nygard/class-dump.git class-dump
 xcodebuild -project ./class-dump/class-dump.xcodeproj #Compile classdump
 mv ./class-dump/build/Release/class-dump /usr/local/bin
+rm -rf ./class-dump
