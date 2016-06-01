3c8c32a1e72ae7bc8c8e041900d38e70de5632fa
Zhang <Naville@users.noreply.github.com>
2016-06-01 14:34:35 +0800

    recursive clone theos

--- resources/scripts/Install Commonly Used Utilities/Installer.sh
+++ resources/scripts/Install Commonly Used Utilities/Installer.sh
@@ -10,7 +10,8 @@ brew install ldid
 echo "Installing Theos"
 echo "export THEOS=/opt/theos" >>~/.bash_profile
 source ~/.bash_profile
-sudo git clone https://github.com/theos/theos.git $THEOS
+export THEOS=/opt/theos
+sudo git clone  --recursive https://github.com/theos/theos.git $THEOS
 sudo git clone https://github.com/theos/headers.git $THEOS/Headers
 sudo cp -r $THEOS/Headers/ $THEOS/include
 sudo rm -rf $THEOS/Headers/
